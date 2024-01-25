7e1c35018aaa150fc3b721672d7ab4129eaf3dd9
allanmark123 <157686475+allanmark123@users.noreply.github.com>
2024-01-25 14:39:55 +0530

    Add files via upload

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2,num3, num4 sum;
+    int num1, num2, num3, num4, num5, num6 sum;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -11,9 +11,11 @@ int main() {
     
     printf("Enter the third number: ");
     scanf("%d",num3);
+
+    printf("Enter the fourth number:");
+    scanf("%d",num4);
     
-    
-    sum = num1 + num2;
+    sum = num1 + num2 + num3 + num4;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
